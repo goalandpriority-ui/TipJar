f2d65a56507a0cc58e3a291277d9e0f1f80d9619
goalandpriority-ui <goalandpriority@gmail.com>
2026-02-24 18:25:59 +0530

    Delete 404.js

--- 404.js
+++
@@ -1,7 +0,0 @@
-export default function Custom404() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-5xl font-bold">404 - Page Not Found</h1>
-    </div>
-  );
-}
